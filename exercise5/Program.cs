54c4e62b470f462b95dcd400a4b7c214fc0f7adc
AMalevic <a.malevic87@gmail.com>
2022-03-16 16:49:50 +0300

    Exercise5

---
+++ exercise5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Петров";
+            string lastName = "Иван";
+            string buffer = name;
+            name = lastName;
+            lastName = buffer;
+            Console.WriteLine($"Имя: {name}\nФамилия: {lastName}");
+        }
+    }
+}
