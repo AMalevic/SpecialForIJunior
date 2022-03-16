554f6b448f6e507bcc099a0c1e56980936139bc5
AMalevic <a.malevic87@gmail.com>
2022-03-16 17:10:21 +0300

    Exercise6

---
+++ exercise6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint artСounter = 52;
+            uint rowLimit = 3;
+            uint lineСounter;
+            uint remainder;
+
+            lineСounter = artСounter / rowLimit;
+            remainder = artСounter % rowLimit;
+            Console.WriteLine($"Заполненых рядов: {lineСounter}\n Излишек: {remainder}");
+            Console.ReadKey();
+        }
+    }
+}
