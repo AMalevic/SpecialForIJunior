7baa2dc548503b47f3afdf9709577ad1116fcfc2
AMalevic <a.malevic87@gmail.com>
2022-03-16 13:56:52 +0300

    Start

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo input = Console.ReadKey();
+            string text = "Съешь еще этих мягких французских булок..."; 
+            uint life = 100;
+            bool isAlive = true;
+            float damageFist = 10.5f;
+            char symbol = 'a';
+            int laps = 1;
+            double xPosition = 233214.4324;
+            ulong gold = 18446744073709551615;
+            ushort experience = 41214;
+        }
+    }
+}
